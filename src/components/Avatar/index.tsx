3e405b53fa6efaf97a1665d3b388b2b0585b564e
Dan Cormier <dancormierall@gmail.com>
2021-06-30 12:13:07 -0400

    Make Avatar `letter`  independent of `img`

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -25,11 +25,8 @@ const Avatar = ({
   ...rest
 }: AvatarProps): ReactElement => (
   <View className={`s-avatar s-avatar__${size} ${className}`} {...rest}>
-    {letter ? (
-      <div className="s-avatar--letter">{letter}</div>
-    ) : (
-      <img className="s-avatar--image" src={src} alt={alt} />
-    )}
+    {letter ? <div className="s-avatar--letter">{letter}</div> : ''}
+    {src ? <img className="s-avatar--image" src={src} alt={alt} /> : ''}
     {children}
   </View>
 );
